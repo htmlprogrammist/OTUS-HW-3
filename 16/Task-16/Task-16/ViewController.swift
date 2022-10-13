4922e4ace11404a17cd1716fd551650a841a15dd
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-10-13 20:27:13 +0300

    feat: create empty project for 16th task

---
+++ 16/Task-16/Task-16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task-16
+//
+//  Created by Егор Бадмаев on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
