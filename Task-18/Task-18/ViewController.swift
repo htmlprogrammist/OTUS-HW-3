71f66cd00ee5f7e88b8374a56a363f83ec04118e
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-10-21 12:38:54 +0300

    feat: create project for 18th task

---
+++ Task-18/Task-18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task-18
+//
+//  Created by Егор Бадмаев on 21.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
