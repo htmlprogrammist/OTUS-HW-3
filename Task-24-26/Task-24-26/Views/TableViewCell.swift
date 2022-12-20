60996ebd72ecb9c472285fbfe255547051bb3ad2
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-12-21 00:19:48 +0300

    feat: implement table view cell

--- Task-24-26/Task-24-26/Views/TableViewCell.swift
+++ Task-24-26/Task-24-26/Views/TableViewCell.swift
@@ -9,6 +9,21 @@ import UIKit
 
 final class TableViewCell: UITableViewCell {
     
+    private let todosImageView = UIImageView()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        return label
+    }()
+    
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .secondaryLabel
+        return label
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
@@ -20,10 +35,25 @@ final class TableViewCell: UITableViewCell {
     }
     
     public func configure(with data: Todo) {
-        
+        if data.completed {
+            todosImageView.tintColor = .systemGreen
+            todosImageView.image = UIImage(systemName: "checkmark.circle")
+        } else {
+            todosImageView.tintColor = .systemRed
+            todosImageView.image = UIImage(systemName: "circle")
+        }
+        titleLabel.text = data.title
+        subtitleLabel.text = "ID: \(data.id), user ID: \(data.userID)"
     }
     
     private func setupView() {
+        let labelsStackView = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel])
+        labelsStackView.spacing = 4
+        labelsStackView.axis = .vertical
         
+        let mainStackView = UIStackView(arrangedSubviews: [todosImageView, labelsStackView])
+        mainStackView.spacing = 4
+        mainStackView.axis = .horizontal
+        contentView.addSubview(mainStackView)
     }
 }
